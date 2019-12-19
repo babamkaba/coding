3ca971d817b2aba9635c29bfda5172c8b622895d
babam <babamkaba@yahoo.fr>
2019-12-19 14:54:22 +0100

    MAJ du service Paire Possible (A-Z) , l'ordre pas important

--- formation/src/test/java/formation/PairePossibleTest.java
+++ formation/src/test/java/formation/PairePossibleTest.java
@@ -2,17 +2,40 @@ package formation;
 
 import org.junit.Test;
 
+import formation.exception.PaireException;
 import formation.service.PairePossible;
 import junit.framework.TestCase;
 
 public class PairePossibleTest extends TestCase {
 	
+	public static final String MOINS_CARACTERS = "Le nombre de paires ne peut être constitué qu'avec deux caractères au moins";
+	public static final String PLUS_CARACTERS = "Le nombre de caracteres possibles est 27";
+	
 	@Test
 	public void testPairePossiblePositif(){
 		
 		PairePossible paire = new PairePossible();		
-		int k = paire.nombrePaire(6);
-		assertEquals(15, k);
+		int k = paire.count(3);
+		assertEquals(3, k);
+		
+	}	
+	@Test
+	public void testPairePossibleVingtSept(){
+		
+		PairePossible paire = new PairePossible();		
+		int k = paire.count(27);
+		assertEquals(351, k);
+	}	
+	@Test(expected=PaireException.class)
+	public void testPairePossibleSupVingtSept(){		
+		PairePossible paire = new PairePossible();		
+		paire.count(Integer.MAX_VALUE);			
+	
+	}	
+	@Test(expected=PaireException.class)
+	public void testPairePossibleInfDeux(){		
+		PairePossible paire = new PairePossible();		
+		paire.count(Integer.MIN_VALUE);
 		
 	}
 
---
+++ formation/src/test/java/formation/exception/PaireException.java
@@ -0,0 +1,11 @@
+package formation.exception;
+
+public class PaireException extends IllegalArgumentException{
+
+	private static final long serialVersionUID = 1L;
+
+	public PaireException(String s) {
+		super(s);		
+	}
+
+}
